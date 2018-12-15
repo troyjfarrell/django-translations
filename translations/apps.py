a352e92d0d42f20a033920cbd1409ab3b50e4685
bbmokhtari <35877268+bbmokhtari@users.noreply.github.com>
2018-12-15 10:05:35 +0330

    `performance` separate fields created for each translatable field

--- translations/apps.py
+++ translations/apps.py
@@ -7,10 +7,16 @@ class TranslationsConfig(AppConfig):
     verbose_name = _('translations')
 
     def ready(self):
+        print('ready')
         try:
             # cache all content types at the start
             from django.contrib.contenttypes.models import ContentType
             models = [ct.model_class() for ct in ContentType.objects.all()]
             ContentType.objects.get_for_models(*models)
+
+            from django.db import models as m
+            for model in models:
+                field = m.CharField('trans', max_length=255)
+                field.contribute_to_class(model, 'trans')
         except Exception:
-            pass
+            raise
